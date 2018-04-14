152264f9a0a19fb270190d205bcc9b417920bd6b
James <jwprillaman@gmail.com>
2018-04-14 15:20:00 -0600

    basic server set to ui folder

--- block-tax.go
+++ block-tax.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-
+	fmt.Printf("Starting server on port 8080")
+	http.Handle("/", http.FileServer(http.Dir("./ui")))
+	http.ListenAndServe(":8080", nil)
 }
