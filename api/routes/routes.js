4841e1b3a3039651b9e7a83d4ce145c9a5291a0c
James <jwprillaman@gmail.com>
2018-04-15 04:30:00 -0600

    created supported list

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,12 +2,14 @@ var http = require("http");
 var https = require("https");
 var Binance = require('binance-api-node').default;
 
+var SUPPORTED_MARKETS = ["LTCBTC", "BNBBTC", "BTCLTC", "BTCETH" , "ETHBTC", "BTCBNB"];
+
 var apiRouter = function(api, marketToBaseAsset) {
 
-	api.post("/getTransactions", async function(req,res){
+	api.post("/getSummary", async function(req,res){
 		var publicKey = req.body.binancePublic;
 		var secretKey = req.body.binanceSecret;
-
+		var binanceTransactions = [];
 		var payload = {
 			"protocol": "https:",
 			"host": "www.binance.com",
@@ -18,15 +20,21 @@ var apiRouter = function(api, marketToBaseAsset) {
 			"apiSecret": secretKey
 		});
 
-		var markets = Object.keys(marketToBaseAsset)
-		console.log("time to make " + markets.length);
+		var deposits = (await binance.depositHistory()).depositList;
+		console.log(deposits);
+
+		var markets = Object.keys(marketToBaseAsset).filter(function(a){
+			return (SUPPORTED_MARKETS.indexOf(a) !== -1);
+		});
+		console.log(markets);
 		for(var i = 0; i < markets.length; i++){
-			console.log(await binance.allOrders({
+			console.log("Getting transactions for  : " + markets[i] );
+			binanceTransactions = binanceTransactions.concat(await binance.allOrders({
 				symbol: markets[i],
 				useServerTime: true
-			}))
+			}));
 		}
-		
+
 	})
 
 }
