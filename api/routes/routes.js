043f7e57a118cc014ac8a683e9650eb6a83e6e69
James <jwprillaman@gmail.com>
2018-04-15 06:00:56 -0600

    get withdraws and deposits into standin transactions

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,14 +2,52 @@ var http = require("http");
 var https = require("https");
 var Binance = require('binance-api-node').default;
 
-var SUPPORTED_MARKETS = ["LTCBTC", "BNBBTC", "BTCLTC", "BTCETH" , "ETHBTC", "BTCBNB"];
+var SUPPORTED_MARKETS = ["LTCBTC", "BNBBTC", "BTCLTC", "BTCETH", "ETHBTC", "BTCBNB"];
 
-var apiRouter = function(api, marketToBaseAsset) {
+function depositsToTransactions(deposits) {
+	var output = [];
+	for (var i = 0; i < deposits.length; i++) {
+		if (deposits[i].status === 1) {
+			output.push({
+				symbol: "USD" + deposits[i].asset,
+				orderId: null,
+				price: null,
+				origQty: deposits[i].amount,
+				executedQty: deposits[i].amount,
+				time: deposits[i].insertTime,
+				type: "LIMIT",
+				side: "BUY",
+			})
+		}
+	}
+	return output;
+}
+function withdrawsToTransactions(withdraws) {
+	var output = [];
+	for (var i = 0; i < withdraws.length; i++) {
+		if (withdraws[i].status === 6) {
+			output.push({
+				symbol: withdraws[i].asset + "USD",
+				orderId: null,
+				price: null,
+				origQty: withdraws[i].amount,
+				executedQty: withdraws[i].amount,
+				time: withdraws[i].insertTime,
+				type: "LIMIT",
+				side: "BUY",
+			})
+		}
+	}
+	return output;
+}
 
-	api.post("/getSummary", async function(req,res){
+var apiRouter = function (api, marketToBaseAsset) {
+
+	api.post("/getSummary", async function (req, res) {
 		var publicKey = req.body.binancePublic;
 		var secretKey = req.body.binanceSecret;
 		var binanceTransactions = [];
+		var promises = [];
 		var payload = {
 			"protocol": "https:",
 			"host": "www.binance.com",
@@ -19,24 +57,31 @@ var apiRouter = function(api, marketToBaseAsset) {
 			"apiKey": publicKey,
 			"apiSecret": secretKey
 		});
+		promises.push(Promise.all([binance.depositHistory(), binance.withdrawHistory()]).then(function(responses){
+			return (new Promise(function(resolve, reject){
+				var output = depositsToTransactions(responses[0].depositList);
+				output = output.concat(withdrawsToTransactions(responses[1].withdrawList));
+				resolve(output);
+			}));
+		}));
 
-		var deposits = (await binance.depositHistory()).depositList;
-		console.log(deposits);
-
-		var markets = Object.keys(marketToBaseAsset).filter(function(a){
+		var markets = Object.keys(marketToBaseAsset).filter(function (a) {
 			return (SUPPORTED_MARKETS.indexOf(a) !== -1);
 		});
-		console.log(markets);
-		for(var i = 0; i < markets.length; i++){
-			console.log("Getting transactions for  : " + markets[i] );
-			binanceTransactions = binanceTransactions.concat(await binance.allOrders({
+		for (var i = 0; i < markets.length; i++) {
+			console.log("Getting transactions for  : " + markets[i]);
+			promises.push(binance.allOrders({
 				symbol: markets[i],
 				useServerTime: true
 			}));
 		}
 
-	})
-
+		Promise.all(promises).then(function (currentTransactions) {
+			for(var i = 0; i < currentTransactions.length; i++){
+				binanceTransactions = binanceTransactions.concat(currentTransactions[i]);
+			}
+		});
+	});
 }
 
 module.exports = apiRouter;
